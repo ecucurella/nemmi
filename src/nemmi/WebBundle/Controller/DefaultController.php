0e524ed73837c2a26a6b0938f1833755caecc946
Joan Teixidó <joan@laiogurtera.com>
2013-11-09 17:36:43 +0100

    resolved problems with case letter

--- src/nemmi/WebBundle/Controller/DefaultController.php
+++ src/nemmi/WebBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace nemmi\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use nemmi\WebBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('nemmiWebBundle:Default:index.html.twig', array('name' => $name));
+
+
+        $user = new User();
+        $user->setName('Bob');
+        $user->setEmail('bob@tv3.cat');
+        $user->setPassword(1234);
+        $user->setLocality('Barcelona');
+
+        $em = $this->getDoctrine();
+        $em->persist($user);
+        $em->flush();
+
+
     }
 }
